f2a1f6debe72f48b362139361252377d097c0177
BielakPrzemyslaw <przemyslawbielak@interia.pl>
2019-12-23 15:04:46 +0100

    Demo calculator test 2

--- src/main/java/com/kodilla/Calculator.java
+++ src/main/java/com/kodilla/Calculator.java
@@ -1,4 +1,20 @@
 package com.kodilla;
+class SimpleSubstract
+{
+    public double substractBFromA(double a, double b) {
 
-public class Calculator {
+        return a * b;
+    }
+}
+
+class Aplication
+{
+    public static void main (String[] args) {
+
+        SimpleSubstract calculator = new SimpleSubstract();
+
+        double result = calculator.substractBFromA(16, 2);
+
+        System.out.println(result);
+    }
 }
